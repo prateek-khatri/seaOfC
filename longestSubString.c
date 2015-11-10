73ce4b8ae71335b10b6036d351db1dea0a35aa19
Prateek Khatri <prateek.khatri90@gmail.com>
2015-11-10 01:50:29 -0800

    Did't Work out need to redo

--- longestSubString.c
+++ longestSubString.c
@@ -2,59 +2,17 @@
  * Find the Longest Common Substring
  * in Two Given Strings X and Y using
  * DYNAMIC PROGRAMMING
- * This program is a trail to use
- * Longest Common Subsequence Table
- * to find the longest substring
  ***************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX(a,b) ((a>b)?(a):(b))
 
 
 void findLCS(char *x,char *y,int m,int n)
 {
-	int L[m+1][n+1]; // Table for Dynamic Approach
-	int i,j;
-	for(i=0;i<=m;i++) //Set first Row as 0
-	{
-		L[i][0] = 0;
-	}
-	for(j=0;j<=n;j++) //Set first Column as 0
-	{
-		L[0][j] = 0;
-	}
 	
-	for(i=1;i<=m;i++)
-	{
-		for(j=1;j<=n;j++)
-		{
-			if(x[i-1] == y[j-1]) 
-			{	//If Match Add 1 to previous diagonal
-				L[i][j] = 1 + L[i-1][j-1];
-			}
-			else
-			{	//If no match look for max from top and left
-				L[i][j] = MAX(L[i-1][j],L[i][j-1]);
-			}
-		}
-	}
-	//String to Store LCS
-	char substr[L[m][n]];
-	int substr_size = L[m][n] - 1;
-	////////BACKTRACK TO FIND SUBSTRING///////////////////
-	for(i=m;i>=0;i--)
-	{
-		for(j=n;j>0;j--)
-		{
-			if(L[i][j] > L[i][j-1])
-			{
-				substr[seq_size--] = y[j-1];
-				i--;
-			}
-		}
-	}
-	printf("\nThe Longest Common Sub-String is: %s\n\n",substr);
 	
 }
 
